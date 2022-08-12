469bffd1ebec383db9fb12ba26f8b2731dc0b868
Mizanur <jameslev2001@gmail.com>
2022-08-12 15:37:58 +0530

    add comment box in frontend side

---
+++ frontend/src/Data/commentData.js
@@ -0,0 +1,27 @@
+export const comments = [
+    {
+        userName: "@nilu",
+        comment: 'This is first Date with My huhuhuh...'
+    },
+    {
+        userName: "@jameslev2001",
+        comment: 'got it toke ato expalin kor te hobe na'
+    },
+    {
+        userName: "@sruti@99",
+        comment: 'enjoy kore chis??..'
+    },
+    {
+        userName: "@test2001",
+        comment: 'wish for the next .....'
+    },
+    {
+        userName: "@test20010",
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos accusamus iusto quis molestias commodi architecto ab, quaerat neque et labore earum. Rem blanditiis voluptate laboriosam qui ut harum ipsam atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos accusamus iusto quis molestias commodi architecto ab, quaerat neque et labore earum. Rem blanditiis voluptate laboriosam qui ut harum ipsam atque.'
+
+    },
+    {
+        userName: "@test2001",
+        comment: 'wish for the next .....'
+    }
+]
--- frontend/src/components/MiddleSide/Post/Post.jsx
+++ frontend/src/components/MiddleSide/Post/Post.jsx
@@ -5,11 +5,13 @@ import ShareImg from '../../../img/share.png'
 import CommentImg from '../../../img/comment.png'
 import axios from 'axios'
 import './Post.css'
+import { comments } from '../../../Data/commentData'
 
 
 const Post = (data, id) => {
     const [liked, setLiked] = useState(data.data.likes.includes(localStorage.getItem('userId')))
     const [noLikes, setNoLiks] = useState(data.data.likes.length)
+    const [showComments, setShowComments] = useState(false)
 
     const funcLiked = () => {
         const API = axios.create({ baseURL: `http://localhost:5000` })
@@ -24,6 +26,10 @@ const Post = (data, id) => {
         liked ? setNoLiks((prev) => prev - 1) : setNoLiks((prev) => prev + 1)
     }
 
+    const funcComment = () => {
+        setShowComments((prev) => !prev)
+    }
+
     return (
         <div className='Post'>
             <div className="detail">
@@ -36,11 +42,31 @@ const Post = (data, id) => {
 
             <div className="PostReact">
                 <img src={liked ? LikeImg : NotlikeImg} alt="likeImg" onClick={funcLiked} />
-                <img src={CommentImg} alt="likeImg" />
+                <img src={CommentImg} alt="likeImg" onClick={funcComment} />
                 <img src={ShareImg} alt="likeImg" />
             </div>
 
             <span className='Likes'>{noLikes} likes</span>
+
+            {showComments && <div className='commentBox'>
+                <h4>comments(5)</h4>
+                <div className="allComments">
+                    {comments.map((comment, id) => {
+                        return (
+                            <div key={id}>
+                                <div className='comment'><b>{comment.userName} : </b>
+                                    {comment.comment}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <hr />
+                <div className='commentSender'>
+                    <input type="text" autoComplete='off' placeholder='Enter you comment' />
+                    <button className='btn commentBtn'>Comment</button>
+                </div>
+            </div>}
         </div>
     )
 }
