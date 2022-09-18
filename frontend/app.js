cb4dfdf852f7e97291f5ad1d6f58d3921b40240a
Mizanur <jameslev2001@gmail.com>
2022-09-18 21:12:12 +0530

    Added app.js file into frontend

---
+++ frontend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/')
+app.listen(9000, () => {
+    console.log("listen on port 9000")
+});
